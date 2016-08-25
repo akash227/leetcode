df17a461a4bb177d11487084387bdad4004d9f52
akash227 <akash227@yahoo.com>
2016-08-25 19:44:47 +0530

    Modifing move_zeroes.py

--- move_zeroes.py
+++ move_zeroes.py
@@ -24,3 +24,24 @@ class Solution(object):
                         nums[itr] = nums[i]
                         nums[i] = t
                         break
+
+"""
+Optimal solution
+"""
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        numZeroes = 0
+        lenn = len(nums)
+        for i in range(lenn):
+            if nums[i] == 0:
+                numZeroes += 1
+            else:
+                nums[i - numZeroes] = nums[i]
+        while numZeroes > 0:
+            nums[lenn-numZeroes] = 0
+            numZeroes -= 1
