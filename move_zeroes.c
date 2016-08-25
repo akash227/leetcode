3f7fb7594687f77d30ebbf6c2085d5307b3fb88c
akash227 <akash227@yahoo.com>
2016-08-25 19:54:23 +0530

    Adding move_zeroes.c

--- move_zeroes.c
+++ move_zeroes.c
@@ -6,6 +6,32 @@ Question: https://leetcode.com/problems/move-zeroes/
 
 */
 
+/*
+	Easy to understand solution
+*/
+
+void moveZeroes(int* nums, int numsSize) {
+    int i,j;
+    
+    for(i = 0; i < numsSize; i++){
+        
+        if( nums[i] == 0 ){
+            
+            for(j = i+1; j < numsSize; j++){
+                
+                if(nums[j] != 0){
+                    nums[i] = nums[j];
+                    nums[j] = 0;
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+}
+
 
 
 /*
