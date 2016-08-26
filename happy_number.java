de11716a55d885a809e033586fb688b803c0fe33
akash227 <akash227@yahoo.com>
2016-08-26 20:10:14 +0530

    Modifying happy_number.java

--- happy_number.java
+++ happy_number.java
@@ -2,7 +2,9 @@
 
 202. Happy Number
 
-Question:
+Question: https://leetcode.com/problems/happy-number/
+
+Hint: We have to detect for a cycle and terminate if one occurs
 
 */
 
@@ -47,3 +49,46 @@ public class Solution {
 
 }
 
+/*
+
+Optimal solution
+
+*/
+
+
+public class Solution {
+
+    static int squareSum(int num){
+
+        int sum = 0;
+
+        while(num > 0){
+            int r = num % 10;
+            sum += r * r;
+            num /= 10;
+        }
+
+        return sum;
+
+    }
+
+    public boolean isHappy(int n) {
+        
+        int slow = n, fast = squareSum(n);
+        
+        if(n == 1){
+            return true;
+        }
+        
+        while(slow != fast){
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+            if(fast == 1){
+                return true;
+            }
+        }
+        return slow == 1;
+
+    }
+
+}
